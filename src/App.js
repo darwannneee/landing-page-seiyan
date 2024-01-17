16edc3878655e44bcc8fabc60f08011570d5370e
darwannneee <darwanchannel1@gmail.com>
2024-01-18 01:36:46 +0700

    changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import bgVideo from "./assets/img/bg.mp4"
 import bgMobile from "./assets/img/bgmobile.mp4"
 
+
+import Pallet from "./assets/img/PALLET.png"
+import Dagora from "./assets/img/DAGORA.png"
+import Ballz from "./assets/img/$Ballz.png"
+
 function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
@@ -24,14 +29,22 @@ function App() {
     
 
   return (
-    <div className="App">
+    <div className="App relative h-screen">
       <video autoPlay playsInline loop muted className="w-full h-full object-cover absolute top-0 left-0">
         <source src={isMobile ? bgMobile : bgVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className="text-white text-center absolute">
+          Your browser does not support the video tag.
+        </video>
+      <div className="text-white text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pt-[370px]">
         {/* Add your content on top of the video background */}
-        <h1 className="text-4xl font-bold text-white"></h1>
+        <div className="flex mx-auto">
+          <img src={Pallet} className="w-60 h-[70px] mx-5" alt="Pallet" />
+          <img src={Dagora} className="w-60 h-[70px]" alt="Dagora" />
+        </div>
+      </div>
+
+      <div className="text-white text-center absolute -bottom-8 -right-20 transform -translate-x-1/2 -translate-y-1/2">
+        {/* Add your content on top of the video background */}
+        <img src={Ballz} className="w-60 h-[100px]" alt="Dagora" />
       </div>
     </div>
   );
